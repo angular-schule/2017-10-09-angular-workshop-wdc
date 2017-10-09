45de2f7898a3a2c8a5830634d4356c3141a189e0
Ferdinand Malcher <mail@ferdinand-malcher.de>
2017-10-09 18:20:01 +0200

    Spielerei: Async-Pipe

--- book-rating/src/app/book-details/book-details.component.ts
+++ book-rating/src/app/book-details/book-details.component.ts
@@ -1,5 +1,9 @@
+import { Book } from '../shared/book';
+import { Observable } from 'rxjs/Rx';
+import { BookStoreService } from '../shared/book-store.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import 'rxjs/add/observable/interval';
 
 @Component({
   selector: 'br-book-details',
@@ -8,14 +12,16 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class BookDetailsComponent implements OnInit {
 
+  books$: Observable<any>;
   isbn: string;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private bs: BookStoreService) { }
 
   ngOnInit() {
     console.log('ngOnInit()');
     // this.isbn = this.route.snapshot.params.isbn;
     this.route.params.subscribe(params => this.isbn = params.isbn);
+    this.books$ = Observable.interval(1000);
   }
 
 }
